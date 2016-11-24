83a910d9100d2ad5b400aae2ffe864f6b49d2c6c
rahulraghu94 <rahulraghu94@gmail.com>
2016-11-24 19:48:39 +0530

    Added code to 'begin' and 'end' each databse transaction

--- database-setup.py
+++ database-setup.py
@@ -4,5 +4,6 @@ db = pg.DB(dbname = "home-project-database", user = "rahul")
 
 # Create the tables in the Database
 # Create a User table
+db.query('begin')
 db.query("create table users(id serial primary key, name varchar(250), data varchar(1000))")
-
+db.query('end')
