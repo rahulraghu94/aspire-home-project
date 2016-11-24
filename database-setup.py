2a5b7df20bebd9d8bb70d61029347c0fde665c83
rahulraghu94 <rahulraghu94@gmail.com>
2016-11-25 00:46:27 +0530

    Added extra attribute to the data base: 	Database now holds default value DEFAULT 	Data base also holds last time modified (in string for simplicity)

--- database-setup.py
+++ database-setup.py
@@ -5,5 +5,7 @@ db = pg.DB(dbname = "home-project-database", user = "rahul")
 # Create the tables in the Database
 # Create a User table
 db.query('begin')
-db.query("create table users(id serial primary key, name varchar(250), data varchar(1000))")
+
+db.query("create table users(id serial primary key, name varchar(250), email varchar(250), picture varchar (250), data varchar(2000) DEFAULT 'DEFAULT', time varchar(30))")
+
 db.query('end')
